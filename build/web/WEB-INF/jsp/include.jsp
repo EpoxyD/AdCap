7c6f9af41d712c45c36d5e3057903885309524a1
Karsten <karstengielis@gmail.com>
2016-06-10 10:01:42 +0200

    Skelleton with MVC Spring framework

---
+++ build/web/WEB-INF/jsp/include.jsp
@@ -0,0 +1,9 @@
+<%-- 
+    Document   : include
+    Created on : 09-Jun-2016, 17:03:15
+    Author     : Karsten
+--%>
+
+<%@ page session="false"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
---
+++ src/java/adcap/web/MainController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adcap.web;
+
+import org.springframework.web.servlet.ModelAndView; 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory; 
+import java.util.Date; 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Karsten
+ */
+@Controller
+@RequestMapping("/store/*")
+
+public class MainController {
+    protected final Log logger = LogFactory.getLog(getClass()); 
+    @RequestMapping(method=RequestMethod.GET)
+
+    public ModelAndView test(){
+        String now = (new Date()).toString();
+        logger.info("Returning exampleView view with " + now); 
+        return new ModelAndView("exampleView", "now", now);
+    } 
+    
+}
